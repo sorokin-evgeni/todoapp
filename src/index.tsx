58725ba54983c6c417eda025674f4d109d0415f0
Евгений Сорокин <40in@yandex-team.ru>
2021-06-13 12:36:15 +0300

    react & typescript integration

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+ReactDOM.render((
+    <div>
+        <h1>React is working</h1>
+    </div>
+), document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,22 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: './src/index.tsx',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
     },
-    plugins: [new HtmlWebpackPlugin()]
+    plugins: [new HtmlWebpackPlugin()],
+    module: {
+        rules: [{
+            test: /\.tsx?$/,
+            use: 'ts-loader',
+        }]
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'dist')
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js']
+    }
 };
